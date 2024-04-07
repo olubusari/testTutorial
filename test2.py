affc5f546e0488bf4a5fcf18bf3bcd5d1e740a7a
Oludare Busari <132385079+olubusari@users.noreply.github.com>
2024-04-06 21:14:23 -0600

    Test create a file on a branch

---
+++ test2.py
@@ -0,0 +1,22 @@
+
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.0.31903.59
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "codele", "codele\codele.csproj", "{1CCA9FB1-4AF7-486C-A606-860D18CD3A73}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{1CCA9FB1-4AF7-486C-A606-860D18CD3A73}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{1CCA9FB1-4AF7-486C-A606-860D18CD3A73}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{1CCA9FB1-4AF7-486C-A606-860D18CD3A73}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{1CCA9FB1-4AF7-486C-A606-860D18CD3A73}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+EndGlobal
